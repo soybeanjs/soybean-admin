36e0e669fd9b10df871531400cd9a44a5c5a4ad2
Soybean <honghuangdc@gmail.com>
2021-11-11 00:28:21 +0800

    refactor(projects): 地图插件页面布局重构

--- src/views/component/map/components/TencentMap.vue
+++ src/views/component/map/components/TencentMap.vue
@@ -1,21 +1,15 @@
 <template>
-  <div ref="wrapRef" class="w-full h-full">
-    <div ref="domRef" class="dom-ref-height"></div>
-  </div>
+  <div ref="domRef"></div>
 </template>
 
 <script setup lang="ts">
-import { ref, computed, onMounted } from 'vue';
-import { useScriptTag, useElementSize } from '@vueuse/core';
+import { ref, onMounted } from 'vue';
+import { useScriptTag } from '@vueuse/core';
 import { TENCENT_MAP_SDK_URL } from '@/settings';
 
 const { load } = useScriptTag(TENCENT_MAP_SDK_URL);
 
-const wrapRef = ref<HTMLDivElement | null>(null);
 const domRef = ref<HTMLDivElement | null>(null);
-
-const { height: wrapRefHeight } = useElementSize(wrapRef);
-const domRefHeight = computed(() => `${wrapRefHeight.value}px`);
 
 async function renderBaiduMap() {
   await load(true);
@@ -32,8 +26,4 @@ onMounted(() => {
   renderBaiduMap();
 });
 </script>
-<style scoped>
-.dom-ref-height {
-  height: v-bind(domRefHeight);
-}
-</style>
+<style scoped></style>
