d5bce26454c7d7c9da29e01675624f985755779f
Yanbowen <349952469@qq.com>
2021-11-18 13:37:30 +0800

    feat(projects): 增加Icon以及打印功能示例

--- src/router/modules/feat.ts
+++ src/router/modules/feat.ts
@@ -3,8 +3,12 @@ import { EnumRoutePath, EnumRouteTitle } from '@/enum';
 import { BasicLayout } from '@/layouts';
 import { ROUTE_NAME_MAP, setRouterCacheName } from '@/utils';
 import FeatCopy from '@/views/feat/copy/index.vue';
+import FeatIcon from '@/views/feat/icon/index.vue';
+import FeatPrint from '@/views/feat/print/index.vue';
 
 setRouterCacheName(FeatCopy, ROUTE_NAME_MAP.get('feat_copy'));
+setRouterCacheName(FeatIcon, ROUTE_NAME_MAP.get('feat_icon'));
+setRouterCacheName(FeatPrint, ROUTE_NAME_MAP.get('feat_print'));
 
 const FEAT: CustomRoute = {
   name: ROUTE_NAME_MAP.get('feat'),
@@ -25,6 +29,24 @@ const FEAT: CustomRoute = {
         requiresAuth: true,
         title: EnumRouteTitle.feat_copy,
         fullPage: true
+      }
+    },
+    {
+      name: ROUTE_NAME_MAP.get('feat_icon'),
+      path: EnumRoutePath.feat_icon,
+      component: FeatIcon,
+      meta: {
+        requiresAuth: true,
+        title: EnumRouteTitle.feat_icon
+      }
+    },
+    {
+      name: ROUTE_NAME_MAP.get('feat_print'),
+      path: EnumRoutePath.feat_print,
+      component: FeatPrint,
+      meta: {
+        requiresAuth: true,
+        title: EnumRouteTitle.feat_print
       }
     }
   ]
---
+++ src/views/feat/print/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <n-card title="打印" class="shadow-sm rounded-16px">
+      <n-button type="primary" class="mr-10px" @click="printTable">打印表格</n-button>
+      <n-button type="primary" @click="printImage">打印图片</n-button>
+      <template #footer>
+        <github-link label="printJS：" link="https://github.com/crabbly/Print.js" class="mt-10px" />
+      </template>
+    </n-card>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { NCard, NButton } from 'naive-ui';
+import printJS from 'print-js';
+import { GithubLink } from '@/components';
+
+function printTable() {
+  printJS({
+    printable: [
+      { name: 'soybean', wechat: 'honghuangdc', remark: '欢迎来技术交流' },
+      { name: 'soybean', wechat: 'honghuangdc', remark: '欢迎来技术交流' }
+    ],
+    properties: ['name', 'wechat', 'remark'],
+    type: 'json'
+  });
+}
+function printImage() {
+  printJS({
+    printable: [
+      'https://raw.githubusercontent.com/honghuangdc/project-assets/main/img/qq_qrcode.JPG',
+      'https://raw.githubusercontent.com/honghuangdc/project-assets/main/img/qq_qrcode.JPG'
+    ],
+    type: 'image',
+    header: 'Multiple Images',
+    imageStyle: 'width:100%;'
+  });
+}
+</script>
+<style scoped></style>
