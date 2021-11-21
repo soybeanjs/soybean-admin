2fdb5f563f7d9fa00d8e5343d992342ff34e3a5a
Soybean <honghuangdc@gmail.com>
2021-11-21 16:31:00 +0800

    fix(projects): 布局修复：从填充屏幕高的页面切换至滚动页面导致布局坍塌

--- src/layouts/BasicLayout/components/common/GlobalContent/index.vue
+++ src/layouts/BasicLayout/components/common/GlobalContent/index.vue
@@ -1,19 +1,15 @@
 <template>
-  <div class="flex-1-hidden bg-[#f6f9f8] dark:bg-deep-dark p-16px transition-all duration-300 ease-in-out">
-    <div class="min-h-full" :class="{ 'h-full overflow-hidden': routeProps.fullPage }">
-      <router-view v-slot="{ Component, route }">
-        <transition :name="theme.pageAnimateType" mode="out-in" appear>
-          <keep-alive :include="cacheRoutes">
-            <component
-              :is="Component"
-              v-if="app.reloadFlag"
-              :key="route.fullPath"
-              :class="{ 'h-full': routeProps.fullPage }"
-            />
-          </keep-alive>
-        </transition>
-      </router-view>
-    </div>
+  <div
+    class="flex-1 flex-col-stretch bg-[#f6f9f8] dark:bg-deep-dark p-16px transition-all duration-300 ease-in-out"
+    :class="{ 'overflow-hidden': routeProps.fullPage }"
+  >
+    <router-view v-slot="{ Component, route }">
+      <transition :name="theme.pageAnimateType" mode="out-in" appear>
+        <keep-alive :include="cacheRoutes">
+          <component :is="Component" v-if="app.reloadFlag" :key="route.fullPath" class="flex-1" />
+        </keep-alive>
+      </transition>
+    </router-view>
   </div>
 </template>
 
--- src/layouts/BasicLayout/components/common/SettingDrawer/index.vue
+++ src/layouts/BasicLayout/components/common/SettingDrawer/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <n-drawer v-model:show="app.settingDrawer.visible" :width="330">
+  <n-drawer v-model:show="app.settingDrawer.visible" display-directive="show" :width="330">
     <n-drawer-content title="主题配置" :native-scrollbar="false">
       <dark-mode />
       <nav-mode />
