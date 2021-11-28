9fb641f71e74e054c84cda8e18969d1168ef2903
Soybean <honghuangdc@gmail.com>
2021-11-28 12:16:48 +0800

    fix(components): 修复HorizontalLayout布局

--- src/layouts/BasicLayout/components/HorizontalLayout/index.vue
+++ src/layouts/BasicLayout/components/HorizontalLayout/index.vue
@@ -1,19 +1,17 @@
 <template>
-  <n-layout :native-scrollbar="false" :content-style="scrollbarContentStyle" class="h-full">
+  <n-layout ref="scrollbar" :native-scrollbar="false" :content-style="scrollbarContentStyle" class="h-full">
     <n-layout-header :inverted="headerInverted" :position="headerPosition" :class="{ 'z-11': theme.fixedHeaderAndTab }">
       <global-header :show-logo="true" :show-menu-collape="false" :show-menu="true" class="relative z-2" />
       <global-tab v-if="theme.multiTabStyle.visible" />
     </n-layout-header>
     <space-placeholder />
-    <n-layout-content ref="scrollbar" class="flex-1" :native-scrollbar="false" :content-style="scrollbarContentStyle">
-      <global-content />
-      <global-footer />
-    </n-layout-content>
+    <global-content />
+    <global-footer />
   </n-layout>
 </template>
 
 <script setup lang="ts">
-import { NLayout, NLayoutContent, NLayoutHeader } from 'naive-ui';
+import { NLayout, NLayoutHeader } from 'naive-ui';
 import { useThemeStore } from '@/store';
 import { useLayoutConfig } from '@/composables';
 import { GlobalHeader, GlobalContent, GlobalFooter, GlobalTab, SpacePlaceholder } from '@/layouts/common';
--- src/plugins/assets.ts
+++ src/plugins/assets.ts
@@ -1,8 +1,8 @@
 import 'virtual:windi.css';
-import '../styles/css/global.css';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import '../styles/css/global.css';
 
 /** 引入静态资源 */
 export default function setupAssets() {}
