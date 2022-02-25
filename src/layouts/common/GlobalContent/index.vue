c4546bdfa303f1e89c0d7ddd46b54e4ec5170096
Soybean <honghuangdc@gmail.com>
2022-02-25 10:57:05 +0800

    fix(projects): 修复页面切换动画无变化

--- src/layouts/common/GlobalContent/index.vue
+++ src/layouts/common/GlobalContent/index.vue
@@ -4,7 +4,7 @@
     class="h-full bg-[#f6f9f8] dark:bg-[#101014] transition duration-300 ease-in-out"
   >
     <router-view v-slot="{ Component, route }">
-      <transition name="fade-slide" mode="out-in" appear>
+      <transition :name="theme.page.animateMode" mode="out-in" appear>
         <keep-alive :include="routeStore.cacheRoutes">
           <component :is="Component" v-if="app.reloadFlag" :key="route.path" />
         </keep-alive>
@@ -14,7 +14,7 @@
 </template>
 
 <script setup lang="ts">
-import { useAppStore, useRouteStore } from '@/store';
+import { useAppStore, useThemeStore, useRouteStore } from '@/store';
 
 interface Props {
   /** 显示padding */
@@ -26,6 +26,7 @@ withDefaults(defineProps<Props>(), {
 });
 
 const app = useAppStore();
+const theme = useThemeStore();
 const routeStore = useRouteStore();
 </script>
 <style scoped></style>
