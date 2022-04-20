24010d05fb1ff51cb5e5d94ffe310206a9638711
Soybean <honghuangdc@gmail.com>
2022-04-21 00:26:21 +0800

    feat(projects): 登录页背景图片位置适配移动端

--- src/views/system-view/login/components/LoginBg/index.vue
+++ src/views/system-view/login/components/LoginBg/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="absolute-lt z-1 wh-full overflow-hidden">
-    <div class="absolute -right-300px -top-900px">
+    <div class="absolute -right-300px -top-900px <sm:(-right-100px -top-1170px)">
       <corner-top :start-color="lightColor" :end-color="darkColor" />
     </div>
-    <div class="absolute -left-200px -bottom-400px">
+    <div class="absolute -left-200px -bottom-400px <sm:(-left-100px -bottom-760px)">
       <corner-bottom :start-color="darkColor" :end-color="lightColor" />
     </div>
   </div>
