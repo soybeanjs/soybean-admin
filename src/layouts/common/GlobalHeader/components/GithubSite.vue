e8999144266761b3b701442975c3c00251240d53
Soybean <2570172956@qq.com>
2022-04-27 19:31:40 +0800

    fix(projects): 修复样式

--- src/layouts/common/GlobalHeader/components/GithubSite.vue
+++ src/layouts/common/GlobalHeader/components/GithubSite.vue
@@ -1,10 +1,17 @@
 <template>
-  <hover-container tooltip-content="github" class="w-40px h-full" content-class="hover:text-primary">
-    <a href="https://github.com/honghuangdc/soybean-admin" target="_blank" class="flex-center">
-      <icon-mdi-github class="text-20px" />
-    </a>
+  <hover-container
+    tooltip-content="github"
+    class="w-40px h-full"
+    content-class="hover:text-primary"
+    @click="handleClickLink"
+  >
+    <icon-mdi-github class="text-20px" />
   </hover-container>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+function handleClickLink() {
+  window.open('https://github.com/honghuangdc/soybean-admin', '_blank');
+}
+</script>
 <style scoped></style>
--- src/layouts/common/GlobalHeader/components/MenuCollapse.vue
+++ src/layouts/common/GlobalHeader/components/MenuCollapse.vue
@@ -1,5 +1,5 @@
 <template>
-  <hover-container class="w-40px h-full" @click="app.toggleSiderCollapse">
+  <hover-container class="w-40px h-full" content-class="hover:text-primary" @click="app.toggleSiderCollapse">
     <icon-line-md-menu-unfold-left v-if="app.siderCollapse" class="text-16px" />
     <icon-line-md-menu-fold-left v-else class="text-16px" />
   </hover-container>
