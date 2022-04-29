0243b2750501359a01cba57c8e2c65930f10e40a
Eric_Yuan <330365043@qq.com>
2022-04-29 19:19:10 +0800

    style(GlobalBreadcrumb): 代码格式fix

--- src/layouts/common/GlobalHeader/components/GlobalBreadcrumb.vue
+++ src/layouts/common/GlobalHeader/components/GlobalBreadcrumb.vue
@@ -17,9 +17,9 @@
             :is="breadcrumb.icon"
             v-if="theme.header.crumb.showIcon"
             class="inline-block align-text-bottom mr-4px text-16px"
-            :class="theme.header.inverted && 'text-#BBBBBB'"
+            :class="{ 'text-#BBBBBB': theme.header.inverted }"
           />
-          <span :class="theme.header.inverted && 'text-#BBBBBB'">{{ breadcrumb.label }}</span>
+          <span :class="{ 'text-#BBBBBB': theme.header.inverted }">{{ breadcrumb.label }}</span>
         </template>
       </n-breadcrumb-item>
     </template>
