6a9a362caa9f01f44282618283ded0ed8e2d93b0
Soybean <honghuangdc@gmail.com>
2022-07-07 23:46:07 +0800

    build(projects): 代码优化

--- src/utils/common/index.ts
+++ src/utils/common/index.ts
@@ -3,5 +3,5 @@ export * from './color';
 export * from './number';
 export * from './object';
 export * from './icon';
-export * from './design-pattern';
+export * from './pattern';
 export * from './theme';
