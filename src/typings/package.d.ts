dbd676095b42aaebc783d5c89478306a453195a5
Soybean <honghuangdc@gmail.com>
2022-08-03 22:38:29 +0800

    fix(projects): 修复图标的TS类型

--- src/typings/package.d.ts
+++ src/typings/package.d.ts
@@ -7,3 +7,16 @@ declare namespace BMap {
 }
 
 declare const TMap: any;
+
+declare module 'virtual:icons/*' {
+  import type { FunctionalComponent, SVGAttributes } from 'vue';
+
+  const component: FunctionalComponent<SVGAttributes>;
+  export default component;
+}
+declare module '~icons/*' {
+  import type { FunctionalComponent, SVGAttributes } from 'vue';
+
+  const component: FunctionalComponent<SVGAttributes>;
+  export default component;
+}
