36f06bc8990a13bbd11874ebf651bb7ff283936e
Soybean <honghuangdc@gmail.com>
2022-08-11 14:37:22 +0800

    build(deps): 更新@soybeanjs/eslint-config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,5 @@
 module.exports = {
   extends: ['@soybeanjs'],
-  settings: {
-    'import/resolver': {
-      alias: {
-        map: [
-          ['~', '.'],
-          ['@', './src']
-        ],
-        extensions: ['.js', '.jsx', '.mjs', '.ts', '.tsx', 'mts', '.d.ts']
-      }
-    }
-  },
   rules: {
     'import/no-unresolved': ['error', { ignore: ['uno.css', '~icons/*', 'virtual:svg-icons-register'] }],
     'import/order': [
