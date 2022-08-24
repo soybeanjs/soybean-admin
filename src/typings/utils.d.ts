71a753f323dbca21087511dcfeb0c9225b01c579
Soybean <honghuangdc@gmail.com>
2022-08-24 19:21:22 +0800

    build(projects): 引入TS高级类型库

--- src/typings/utils.d.ts
+++ src/typings/utils.d.ts
@@ -10,4 +10,14 @@ declare namespace TypeUtil {
   type FirstOfArray<T extends any[]> = T extends [infer First, ...infer _Rest] ? First : never;
 
   type LastOfArray<T extends any[]> = T extends [...infer _Rest, infer Last] ? Last : never;
+
+  // union to tuple
+  type Union2IntersectionFn<T> = (T extends unknown ? (k: () => T) => void : never) extends (k: infer R) => void
+    ? R
+    : never;
+  type GetUnionLast<U> = Union2IntersectionFn<U> extends () => infer I ? I : never;
+
+  type UnionToTuple<T, R extends any[] = []> = [T] extends [never]
+    ? R
+    : UnionToTuple<Exclude<T, GetUnionLast<T>>, [GetUnionLast<T>, ...R]>;
 }
