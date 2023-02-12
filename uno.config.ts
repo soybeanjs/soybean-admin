3503dff663416e0a12a502c7f0b17541073443ed
Soybean <honghuangdc@gmail.com>
2023-02-12 23:36:00 +0800

    build(projects): update unocss config

--- uno.config.ts
+++ uno.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from '@unocss/vite';
 import presetUno from '@unocss/preset-uno';
+import transformerDirectives from '@unocss/transformer-directives';
 
 export default defineConfig({
   exclude: ['node_modules', 'dist', '.git', '.husky', '.vscode', 'public', 'build', 'mock', './stats.html'],
   presets: [presetUno({ dark: 'class' })],
+  transformers: [transformerDirectives()],
   shortcuts: {
     'wh-full': 'w-full h-full',
     'flex-center': 'flex justify-center items-center',
