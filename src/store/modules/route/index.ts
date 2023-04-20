5957833a4fd15490c4d67ac2a93504a2d3e75142
Soybean <honghuangdc@gmail.com>
2023-04-21 00:44:40 +0800

    fix(projects): fix router guide [修复路由跳转异常] fixed #216

--- src/store/modules/route/index.ts
+++ src/store/modules/route/index.ts
@@ -107,6 +107,7 @@ export const useRouteStore = defineStore('route-store', {
     },
     /** 初始化动态路由 */
     async initDynamicRoute() {
+      const { resetAuthStore } = useAuthStore();
       const { initHomeTab } = useTabStore();
 
       const { userId } = localStg.get('userInfo') || {};
@@ -125,6 +126,8 @@ export const useRouteStore = defineStore('route-store', {
         initHomeTab(data.home, router);
 
         this.isInitAuthRoute = true;
+      } else {
+        resetAuthStore();
       }
     },
     /** 初始化静态路由 */
