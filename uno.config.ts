0907d38c067b7431da2009e656945122151cbc40
Soybean <honghuangdc@gmail.com>
2023-06-08 23:05:08 +0800

    chore(projects): update deps & update unocss deprecated api exclude

--- uno.config.ts
+++ uno.config.ts
@@ -3,7 +3,11 @@ import presetUno from '@unocss/preset-uno';
 import transformerDirectives from '@unocss/transformer-directives';
 
 export default defineConfig({
-  exclude: ['node_modules', 'dist', '.git', '.husky', '.vscode', 'public', 'build', 'mock', './stats.html'],
+  content: {
+    pipeline: {
+      exclude: ['node_modules', 'dist', '.git', '.husky', '.vscode', 'public', 'build', 'mock', './stats.html']
+    }
+  },
   presets: [presetUno({ dark: 'class' })],
   transformers: [transformerDirectives()],
   shortcuts: {
