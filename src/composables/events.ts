14de157ceffef3388529a952e086be8900819ee3
Ω <1107402337@qq.com>
2023-06-28 16:09:45 +0800

    chore(projects): 页面离开重置theme状态再缓存主题配置。

--- src/composables/events.ts
+++ src/composables/events.ts
@@ -14,6 +14,7 @@ export function useGlobalEvents() {
 
   /** 页面离开时缓存多页签数据 */
   useEventListener(window, 'beforeunload', () => {
+		theme.resetThemeStore()
     theme.cacheThemeSettings();
     tab.cacheTabRoutes();
   });
