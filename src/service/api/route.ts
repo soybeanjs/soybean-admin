2724169eb6f5ff1d328260689b298a381eee9441
Soybean <soybeanjs@outlook.com>
2024-01-26 21:25:16 +0800

    refactor(projects): update mock api

--- src/service/api/route.ts
+++ src/service/api/route.ts
@@ -1,20 +1,15 @@
 import { request } from '../request';
 
-/**
- * Get user routes
- *
- * @param example Whether to use example data, default: 0
- */
-export function fetchGetUserRoutes(example: '0' | '1' = '0') {
-  return request<Api.Route.UserRoute>({ url: '/route/getUserRoutes', params: { example } });
+/** get user routes */
+export function fetchGetUserRoutes() {
+  return request<Api.Route.UserRoute>({ url: '/route/getUserRoutes' });
 }
 
 /**
- * Whether the route is exist
+ * whether the route is exist
  *
- * @param routeName Route name
- * @param example Whether to use example data, default: 0
+ * @param routeName route name
  */
-export function fetchIsRouteExist(routeName: string, example: '0' | '1' = '0') {
-  return request<boolean>({ url: '/route/isRouteExist', params: { routeName, example } });
+export function fetchIsRouteExist(routeName: string) {
+  return request<boolean>({ url: '/route/isRouteExist', params: { routeName } });
 }
