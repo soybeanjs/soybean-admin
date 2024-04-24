667155ae764ba09b08437c888aaf60ba606145f3
Azir <2075125282@qq.com>
2024-04-25 04:44:47 +0800

    feat(projects): Add prefix to local storage

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,7 +1,9 @@
 import { createLocalforage, createStorage } from '@sa/utils';
 
-export const localStg = createStorage<StorageType.Local>('local');
+const StoragePrefix = import.meta.env.VITE_STORAGE_PREFIX || '';
 
-export const sessionStg = createStorage<StorageType.Session>('session');
+export const localStg = createStorage<StorageType.Local>('local', StoragePrefix);
+
+export const sessionStg = createStorage<StorageType.Session>('session', StoragePrefix);
 
 export const localforage = createLocalforage<StorageType.Local>('local');
