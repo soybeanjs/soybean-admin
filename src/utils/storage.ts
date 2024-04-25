81afac449baf219be1ee43ee688370863cfcc342
恕瑞玛的皇帝 <2075125282@qq.com>
2024-04-25 10:39:34 +0800

    fix(projects): Modify the naming of storagePrefix

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,9 +1,9 @@
 import { createLocalforage, createStorage } from '@sa/utils';
 
-const StoragePrefix = import.meta.env.VITE_STORAGE_PREFIX || '';
+const storagePrefix = import.meta.env.VITE_STORAGE_PREFIX || '';
 
-export const localStg = createStorage<StorageType.Local>('local', StoragePrefix);
+export const localStg = createStorage<StorageType.Local>('local', storagePrefix);
 
-export const sessionStg = createStorage<StorageType.Session>('session', StoragePrefix);
+export const sessionStg = createStorage<StorageType.Session>('session', storagePrefix);
 
 export const localforage = createLocalforage<StorageType.Local>('local');
