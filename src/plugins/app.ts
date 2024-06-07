62592872406f2f5ee957cb742b1519661cc16a80
Alue Gitman <51526702+sigma-plus@users.noreply.github.com>
2024-06-07 16:06:33 +0800

    fix(projects): hide AppVersionNotification in DEV mode (#482)

--- src/plugins/app.ts
+++ src/plugins/app.ts
@@ -6,7 +6,7 @@ export function setupAppVersionNotification() {
   document.addEventListener('visibilitychange', async () => {
     const buildTime = await getHtmlBuildTime();
 
-    if (buildTime !== BUILD_TIME && document.visibilityState === 'visible') {
+    if (!import.meta.env.DEV && buildTime !== BUILD_TIME && document.visibilityState === 'visible') {
       const n = window.$notification?.create({
         title: $t('system.updateTitle'),
         content: $t('system.updateContent'),
