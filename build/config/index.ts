069fa8a8178442ae205b127db3d913e3e5dfa80d
Soybean <soybeanjs@outlook.com>
2024-06-13 23:59:30 +0800

    optimize(projects): get buildTime with timezone 'Asia/Shanghai'

--- build/config/index.ts
+++ build/config/index.ts
@@ -1,1 +1,2 @@
 export * from './proxy';
+export * from './time';
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
 import process from 'node:process';
 import { URL, fileURLToPath } from 'node:url';
 import { defineConfig, loadEnv } from 'vite';
-import dayjs from 'dayjs';
 import { setupVitePlugins } from './build/plugins';
-import { createViteProxy } from './build/config';
+import { createViteProxy, getBuildTime } from './build/config';
 
 export default defineConfig(configEnv => {
   const viteEnv = loadEnv(configEnv.mode, process.cwd()) as unknown as Env.ImportMeta;
 
-  const buildTime = dayjs().format('YYYY-MM-DD HH:mm:ss');
+  const buildTime = getBuildTime();
 
   return {
     base: viteEnv.VITE_BASE_URL,
