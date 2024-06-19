8545c4f43640b0fbce5b9e7131744e10db51c712
RockerHX <rockerhx@gmail.com>
2024-06-19 11:27:10 +0800

    Add VITE_ELEGANT_ROUTER to env.d.ts
    Whether to build with ElegantVueRouter

--- src/typings/env.d.ts
+++ src/typings/env.d.ts
@@ -103,6 +103,8 @@ declare namespace Env {
     readonly VITE_ICONIFY_URL?: string;
     /** Used to differentiate storage across different domains */
     readonly VITE_STORAGE_PREFIX?: string;
+    /** Whether to build with ElegantVueRouter */
+    readonly VITE_ELEGANT_ROUTER?: CommonType.YesOrNo;
   }
 }
 
