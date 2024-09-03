4c9f4e09b17784ed4d2eb7a8d1833c0fc5fdcac4
青菜白玉汤 <79054161+Azir-11@users.noreply.github.com>
2024-09-03 16:52:38 +0800

    feat(projects): does the configuration support automatic updates. close#612 (#617)

--- src/typings/env.d.ts
+++ src/typings/env.d.ts
@@ -103,6 +103,8 @@ declare namespace Env {
     readonly VITE_ICONIFY_URL?: string;
     /** Used to differentiate storage across different domains */
     readonly VITE_STORAGE_PREFIX?: string;
+    /** Whether to automatically detect updates after configuring application packaging */
+    readonly VITE_AUTOMATICALLY_DETECT_UPDATE?: CommonType.YesOrNo;
   }
 }
 
