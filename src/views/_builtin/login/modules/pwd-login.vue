86da767e24d851226bbe7cb190b90f3e21022496
李鹏坤-执手对影成双 <mail@lipk.org>
2025-01-25 18:38:49 +0800

    fix(projects): fix multiple calls to the login API when clicking quickly. fixed #697 (#698)

--- src/views/_builtin/login/modules/pwd-login.vue
+++ src/views/_builtin/login/modules/pwd-login.vue
@@ -107,7 +107,13 @@ async function handleAccountLogin(account: Account) {
       </div>
       <NDivider class="text-14px text-#666 !m-0">{{ $t('page.login.pwdLogin.otherAccountLogin') }}</NDivider>
       <div class="flex-center gap-12px">
-        <NButton v-for="item in accounts" :key="item.key" type="primary" @click="handleAccountLogin(item)">
+        <NButton
+          v-for="item in accounts"
+          :key="item.key"
+          :loading="authStore.loginLoading"
+          type="primary"
+          @click="handleAccountLogin(item)"
+        >
           {{ item.label }}
         </NButton>
       </div>
