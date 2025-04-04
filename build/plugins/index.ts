29698bef69ddee728e7bcad2cad18ce1fe57e77a
青菜白玉汤 <79054161+Azir-11@users.noreply.github.com>
2025-04-04 12:00:52 +0800

    feat(projects): support vite devtools specify the editor by launchEditor option. (#730)

--- build/plugins/index.ts
+++ build/plugins/index.ts
@@ -1,18 +1,18 @@
 import type { PluginOption } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
-import VueDevtools from 'vite-plugin-vue-devtools';
 import progress from 'vite-plugin-progress';
 import { setupElegantRouter } from './router';
 import { setupUnocss } from './unocss';
 import { setupUnplugin } from './unplugin';
 import { setupHtmlPlugin } from './html';
+import { setupDevtoolsPlugin } from './devtools';
 
 export function setupVitePlugins(viteEnv: Env.ImportMeta, buildTime: string) {
   const plugins: PluginOption = [
     vue(),
     vueJsx(),
-    VueDevtools(),
+    setupDevtoolsPlugin(viteEnv),
     setupElegantRouter(),
     setupUnocss(viteEnv),
     ...setupUnplugin(viteEnv),
