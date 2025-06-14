206d5a44596dea8515cdc177de8b6db3837382b5
Azir <2075125282@qq.com>
2025-06-14 19:54:43 +0800

    feat(projects): refactor theme drawer with tabbed layout for better UX.

---
+++ src/layouts/modules/theme-drawer/modules/general/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import GlobalSettings from './modules/global-settings.vue';
+
+defineOptions({
+  name: 'GeneralSettings'
+});
+</script>
+
+<template>
+  <div class="flex-col-stretch gap-16px">
+    <GlobalSettings />
+  </div>
+</template>
+
+<style scoped></style>
