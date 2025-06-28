d37ce04606d7edca6e521ff794dcc53bd11b0351
Azir <2075125282@qq.com>
2025-06-28 13:43:36 +0800

    refactor(types): move Auth and Route namespaces to separate files and clean up api.d.ts

--- src/typings/api.d.ts
+++ src/typings/api.d.ts
@@ -47,41 +47,4 @@ declare namespace Api {
       status: EnableStatus | null;
     } & T;
   }
-
-  /**
-   * namespace Auth
-   *
-   * backend api module: "auth"
-   */
-  namespace Auth {
-    interface LoginToken {
-      token: string;
-      refreshToken: string;
-    }
-
-    interface UserInfo {
-      userId: string;
-      userName: string;
-      roles: string[];
-      buttons: string[];
-    }
-  }
-
-  /**
-   * namespace Route
-   *
-   * backend api module: "route"
-   */
-  namespace Route {
-    type ElegantConstRoute = import('@elegant-router/types').ElegantConstRoute;
-
-    interface MenuRoute extends ElegantConstRoute {
-      id: string;
-    }
-
-    interface UserRoute {
-      routes: MenuRoute[];
-      home: import('@elegant-router/types').LastLevelRouteKey;
-    }
-  }
 }
