3d0daae43c576876cf2f92b42e30cde45b831c00
Soybean <soybeanjs@outlook.com>
2025-07-31 00:26:00 +0800

    reafctor(projects): use new elegant-router plugin

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,6 @@
 import type { App } from 'vue';
-import {
-  type RouterHistory,
-  createMemoryHistory,
-  createRouter,
-  createWebHashHistory,
-  createWebHistory
-} from 'vue-router';
-import { createBuiltinVueRoutes } from './routes/builtin';
+import { createMemoryHistory, createRouter, createWebHashHistory, createWebHistory } from 'vue-router';
+import type { RouterHistory } from 'vue-router';
 import { createRouterGuard } from './guard';
 
 const { VITE_ROUTER_HISTORY_MODE = 'history', VITE_BASE_URL } = import.meta.env;
@@ -19,7 +13,7 @@ const historyCreatorMap: Record<Env.RouterHistoryMode, (base?: string) => Router
 
 export const router = createRouter({
   history: historyCreatorMap[VITE_ROUTER_HISTORY_MODE](VITE_BASE_URL),
-  routes: createBuiltinVueRoutes()
+  routes: []
 });
 
 /** Setup Vue Router */
