66631274e5675e7524ac2ac0f6ed05cf0e9da9cd
CyberShen123 <s.lijun@qq.com>
2025-10-02 23:09:25 +0800

    feat(projects): support global radius setting

--- uno.config.ts
+++ uno.config.ts
@@ -23,7 +23,7 @@ export default defineConfig<Theme>({
     }
   },
   shortcuts: {
-    'card-wrapper': 'rd-8px shadow-sm'
+    'card-wrapper': 'shadow-sm'
   },
   transformers: [transformerDirectives(), transformerVariantGroup()],
   presets: [presetWind3({ dark: 'class' }), presetSoybeanAdmin()]
